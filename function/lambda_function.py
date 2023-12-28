a9f0804c4e22cfcaae9029a8c0835a4df6472a0f
jasonalles <jalles@emme-inc.com>
2023-12-28 15:08:20 -0800

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -10,10 +10,4 @@ def lambda_handler(event, context):
     """
     print('Starting functions\n---------------------------------------------'
 
-    if event["input"] == "Hello":
-
-        return "World"
-
-    else:
-
-        raise
+    return 'Hello World'
